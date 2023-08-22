11b68407248c14e044c0a2652bcb921a5a8d467d
Vinicius Gaspar <vinicius.gaspar@csptech.com.br>
2023-08-22 10:30:10 -0300

    fix imports

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/viniciusmgaspar/client-Server_api/client"
-	"github.com/viniciusmgaspar/client-Server_api/database"
-	"github.com/viniciusmgaspar/client-Server_api/server"
+	"github.com/viniciusmgaspar/client_Server_api/client"
+	"github.com/viniciusmgaspar/client_Server_api/database"
+	"github.com/viniciusmgaspar/client_Server_api/server"
 )
 
 func main() {
